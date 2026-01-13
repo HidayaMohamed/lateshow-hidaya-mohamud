bd0a2416c880c7c4bae9e7a60e3f719b3fc60ea3
Hidaya Mohamed <hidayamohamed002@gmail.com>
2026-01-13 07:28:24 +0300

    Add imports and configs in app.py

---
+++ server/app.py
@@ -0,0 +1,9 @@
+#   Imports
+from flask import Flask, request, jsonify
+from flask_migrate import Migrate
+from models import db, Episode, Guest, Appearance
+# App instance
+app = Flask(__name__)
+# Configured the database and stop tracking notification
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
