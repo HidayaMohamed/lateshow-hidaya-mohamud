f002afb8d69c8124729430e43c021a8a4370c81a
Hidaya Mohamed <hidayamohamed002@gmail.com>
2026-01-13 07:25:25 +0300

    Add relationships and validation

--- server/models.py
+++ server/models.py
@@ -8,23 +8,59 @@ db = SQLAlchemy()
 class Episode(db.Model, SerializerMixin):
     __tablename__ = "episodes"
 
+    # Serialization
+    serialize_rules = ("-appearances.episode",)
+
     # Columns
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.String)
     number = db.Column(db.Integer)
 
+    # Relationships
+    appearances = db.relationship(
+        "Appearance",
+        back_populates="episode",
+        cascade="all, delete-orphan"
+    )
+
+    guests = db.relationship(
+        "Guest",
+        secondary="appearances",
+        viewonly=True
+    )
 
 class Guest(db.Model, SerializerMixin):
     __tablename__ = "guests"
+
+    # Serialization
+    erialize_rules = ("-appearances.guest",)
 
     # Columns
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     occupation = db.Column(db.String)
 
+    # Relationships
+    appearances = db.relationship(
+        "Appearance",
+        back_populates="guest",
+        cascade="all, delete-orphan"
+    )
+
+    episodes = db.relationship(
+        "Episode",
+        secondary="appearances",
+        viewonly=True
+    )
+
 
 class Appearance(db.Model, SerializerMixin):
     __tablename__ = "appearances"
+
+    serialize_rules = (
+        "-episode.appearances",
+        "-guest.appearances",
+    )
 
     # Columns
     id = db.Column(db.Integer, primary_key=True)
@@ -40,3 +76,14 @@ class Appearance(db.Model, SerializerMixin):
         db.ForeignKey("guests.id", ondelete="CASCADE"),
         nullable=False
     )
+
+    # Relationship
+    episode = db.relationship("Episode", back_populates="appearances")
+    guest = db.relationship("Guest", back_populates="appearances")
+
+    # Validation for rating to be between 1 and 5 
+    @validates("rating")
+    def validate_rating(self, key, value):
+        if not (1 <= value <= 5):
+            raise ValueError("Rating must be between 1 and 5")
+        return value 
