c0218ee4510172f028c347be525f3ca6ae14fe92
Hidaya Mohamed <hidayamohamed002@gmail.com>
2026-01-13 11:33:37 +0300

    Added styling to AppearnceForm.jsx

--- client/src/components/AppearanceForm.jsx
+++ client/src/components/AppearanceForm.jsx
@@ -1,25 +1,32 @@
 import { useState } from "react";
 
 function AppearanceForm({ episodes, guests, onSuccess }) {
+  // Set state to null for tracking rating input
   const [rating, setRating] = useState("");
+  // State to track the selected episode
   const [episodeId, setEpisodeId] = useState("");
   const [guestId, setGuestId] = useState("");
   const [errors, setErrors] = useState([]);
 
+  // Function to handle form submission
   function handleSubmit(e) {
     e.preventDefault();
+    // clear previous errors
     setErrors([]);
 
+    // Send POST request to backend
     fetch("http://127.0.0.1:5000/appearances", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
+        // convert string to number
         rating: Number(rating),
         episode_id: Number(episodeId),
         guest_id: Number(guestId),
       }),
     })
       .then((res) => {
+        // If the response is not ok, throw an error to catch block
         if (!res.ok) {
           return res.json().then((err) => {
             throw err;
@@ -28,7 +35,9 @@ function AppearanceForm({ episodes, guests, onSuccess }) {
         return res.json();
       })
       .then((data) => {
+        //   Alerts when an appearance is succesfully created
         alert("Appearance created!");
+        // reloads episode with new appearance
         onSuccess(data.episode_id);
       })
       .catch((err) => {
@@ -48,12 +57,16 @@ function AppearanceForm({ episodes, guests, onSuccess }) {
 
       <form onSubmit={handleSubmit}>
         <input
-          placeholder="Rating (1–5)"
+          style={{ width: "100px", height: "30px" }}
+          placeholder="Rating (1-5)"
           value={rating}
           onChange={(e) => setRating(e.target.value)}
         />
 
-        <select onChange={(e) => setEpisodeId(e.target.value)}>
+        <select
+          onChange={(e) => setEpisodeId(e.target.value)}
+          style={{ width: "150px", height: "35px" }}
+        >
           <option value="">Select Episode</option>
           {episodes.map((ep) => (
             <option key={ep.id} value={ep.id}>
@@ -62,7 +75,10 @@ function AppearanceForm({ episodes, guests, onSuccess }) {
           ))}
         </select>
 
-        <select onChange={(e) => setGuestId(e.target.value)}>
+        <select
+          onChange={(e) => setGuestId(e.target.value)}
+          style={{ width: "150px", height: "35px" }}
+        >
           <option value="">Select Guest</option>
           {guests.map((g) => (
             <option key={g.id} value={g.id}>
