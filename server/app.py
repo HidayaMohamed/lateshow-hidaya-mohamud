e40659b7ea4da7bbf6b29a08b7fbf6b025a1684e
Hidaya Mohamed <hidayamohamed002@gmail.com>
2026-01-13 08:52:33 +0300

    Added an insance of mingrate

--- server/app.py
+++ server/app.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 # Configured the database and stop tracking notification
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+db.init_app(app)
+migrate = Migrate(app, db)
